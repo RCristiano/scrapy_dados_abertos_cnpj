a49aadb98bcb6382cbb5a37396276eb25e94f7bb
Rodrigo Vieira <rodrigo.vieira@cetax.com.br>
2019-04-24 04:56:01 -0300

    Verify date and get zip file

--- dados_publicos_cnpj/pipelines.py
+++ dados_publicos_cnpj/pipelines.py
@@ -11,16 +11,27 @@ import urllib.request
 
 class DadosPublicosCnpjPipeline(object):
     def open_spider(self, spider):
-      self.file = open('last_update.json', 'w')
+        self.file = open('last_update.json', 'r+')
 
     def close_spider(self, spider):
-      self.file.close()
+        self.file.close()
 
     def process_item(self, item, spider):
-      line =  json.dumps(dict(item)) + '\n'
-      self.file.write(line)
+        try:
+            archive = json.load(self.file)
 
-      # urllib.request\
-      #     .urlretrieve(item['download_link'],
-      #                 'DADOS_ABERTOS_CNPJ.zip')
-      return item
+            if item['last_update'] == archive['last_update']:
+                return item
+        except:
+            pass
+
+        line =  json.dumps(dict(item))
+        self.file.truncate(0)
+        self.file.seek(0)
+        self.file.write(line)
+
+        urllib.request\
+            .urlretrieve(item['download_link'],
+                        'DADOS_ABERTOS_CNPJ.zip')
+
+        return item
